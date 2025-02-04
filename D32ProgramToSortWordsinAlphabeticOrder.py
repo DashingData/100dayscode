f7e6bc6f6e6d718f6fc87aebbf8f7e91f96f084b
[DashingData] <[abhishekac407@gmail.com]>
2025-02-04 23:46:33 +0530

    code updated

---
+++ D32ProgramToSortWordsinAlphabeticOrder.py
@@ -0,0 +1,29 @@
+a = "Abhishek is a very good boy"
+w = a.split(" ")
+print(w)
+
+for in range(len(w)):
+    w[i] = w[i].lower()
+print(w)
+w.sort()
+print(w)
+
+for i in w:
+    print(i)
+
+
+ ''' by accepting the stirng from the user'''
+
+a = input("Enter the string")
+w = a.split(" ")
+print(w)
+for in range(len(w)):
+    w[i] = w[i].lower()
+print(w)
+w.sort()
+print(w)
+
+for i in w:
+    print(i)
+
+    
